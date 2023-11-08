8a501e53ecb97a341d2040d78d714f06e715b948
deharhur <125957829+deharhur@users.noreply.github.com>
2023-11-08 14:18:44 +0100

    Update spiel.js

--- 4Gewinnt/spiel.js
+++ 4Gewinnt/spiel.js
@@ -1,94 +1,39 @@
-function checkWin(player) {
-  const cells = document.querySelectorAll('.cell');
-  const width = 7;
-  const height = 6;
-
-  function checkDirection(direction) {
-    let count = 0;
-    for (let i = 0; i < direction.length; i++) {
-      const cell = cells[direction[i]];
-      if (cell.classList.contains(player)) {
-        count++;
-        if (count === 4) return true;
-      } else {
-        count = 0;
-      }
-    }
-    return false;
-  }
-
-  for (let row = 0; row < height; row++) {
-    let horizontal = [];
-    for (let col = 0; col < width; col++) {
-      horizontal.push(row * width + col);
-    }
-    if (checkDirection(horizontal)) return true;
-  }
-
-  for (let col = 0; col < width; col++) {
-    let vertical = [];
-    for (let row = 0; row < height; row++) {
-      vertical.push(row * width + col);
-    }
-    if (checkDirection(vertical)) return true;
-  }
-
-  for (let row = 0; row < height - 3; row++) {
-    for (let col = 0; col < width - 3; col++) {
-      let diagonalRight = [];
-      for (let i = 0; i < 4; i++) {
-        diagonalRight.push((row + i) * width + (col + i));
-      }
-      if (checkDirection(diagonalRight)) return true;
-
-      let diagonalLeft = [];
-      for (let i = 0; i < 4; i++) {
-        diagonalLeft.push((row + i) * width + (col + 3 - i));
-      }
-      if (checkDirection(diagonalLeft)) return true;
-    }
-  }
-
-  return false;
-}
-
 function showBoard() {
   const board = document.getElementById('board');
-  board.innerHTML = '';
   let currentPlayer = 'red';
 
   for (let row = 0; row < 6; row++) {
     for (let col = 0; col < 7; col++) {
-      const cell = elt('div', { class: 'cell', 'data-col': col, 'data-row': row });
-      cell.addEventListener('click', () => placePiece(col, currentPlayer), { once: true });
+      const cell = elt('div', {class: 'cell', 'data-col': col, 'data-row': row});
+      cell.addEventListener('click', () => placePiece(col));
       board.appendChild(cell);
     }
   }
 
-  function placePiece(col, player) {
+  function placePiece(col) {
     for (let row = 5; row >= 0; row--) {
       const cell = document.querySelector(`.cell[data-row="${row}"][data-col="${col}"]`);
       if (!cell.hasChildNodes()) {
-        const piece = elt('div', { class: `piece ${player}` });
+        const piece = elt('div', {class: `piece ${currentPlayer}`});
         cell.appendChild(piece);
-        if (checkWin(player)) {
-          alert(`Spieler ${player} hat gewonnen!`);
-          showResetButton();
-        }
-        currentPlayer = currentPlayer === 'red' ? 'blue' : 'red';
+        currentPlayer = (currentPlayer === 'red') ? 'blue' : 'red';
         break;
       }
     }
   }
 }
 
-function showResetButton() {
-  const resetButton = elt('button', {}, 'Spiel zurücksetzen');
-  resetButton.addEventListener('click', () => {
-    showBoard();
-    document.body.removeChild(resetButton);
+function elt(type, attrs, ...children) {
+  const node = document.createElement(type);
+  Object.keys(attrs).forEach(key => node.setAttribute(key, attrs[key]));
+  children.forEach(child => {
+    if (typeof child === 'string') {
+      node.appendChild(document.createTextNode(child));
+    } else {
+      node.appendChild(child);
+    }
   });
-  document.body.appendChild(resetButton);
+  return node;
 }
 
 document.addEventListener('DOMContentLoaded', showBoard);
